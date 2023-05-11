1f8891bd7497e1a8ea4c70c2b93473e58d4dac0e
m.pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2023-05-11 14:00:15 +0200

    my own version wip

--- tests/test_csv_yautay.py
+++ tests/test_csv_yautay.py
@@ -1,19 +1,20 @@
-import json
 import os
-from src.lib.ParserAirbossData import ParserAirbossData
-from src.lib.Bcolors import Bcolors as Colors
+from random import randrange
 from root import ROOT_DIR
-from src.plotters.raw_data_plot.RawDataPlotter import RawDataPlotter
-from src.plotters.yautay_plot.YautayPlotter import YautayPlotter
+from src.lib.ParserCSVAirbossData import ParserCSVAirbossData
+
+test_csv_files = []
+
 
 for filename in os.listdir(os.path.join(ROOT_DIR, "tests", "csv")):
     file_path = os.path.join(os.path.join(ROOT_DIR, "tests", "csv"), filename)
     plot_path = os.path.join(os.path.join(ROOT_DIR, "tests", "results", "data", "raw_plots", "CSV_" + filename.strip(".json")))
     plot_json = os.path.join(os.path.join(ROOT_DIR, "tests", "results", "data", "json", "CSV_" + filename))
+    test_csv_files.append(file_path)
 
-    try:
-        plotter = YautayPlotter(rcvd_data=file_path, dump_parsed_data=plot_json)
-        plotter.plot_case(plot_path)
-    except Exception as e:
-        print(f"{Colors.FAIL} {str(e)} {file_path} {Colors.ENDC}")
+
+x = randrange(len(test_csv_files))
+parser_csv = ParserCSVAirbossData()
+parser_csv.read_csv_trap(test_csv_files[x])
+
 
